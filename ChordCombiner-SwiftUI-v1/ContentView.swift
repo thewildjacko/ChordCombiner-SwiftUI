b2128d587b18b16104f7122604748e48879fe465
Jake Smolowe <jakesmolowe@gmail.com>
2024-06-20 08:27:36 -0700

    Initial Commit

---
+++ ChordCombiner-SwiftUI-v1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChordCombiner-SwiftUI-v1
+//
+//  Created by Jake Smolowe on 6/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
