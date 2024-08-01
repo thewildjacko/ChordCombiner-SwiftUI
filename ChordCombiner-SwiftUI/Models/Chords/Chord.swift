4c26d0d6ab51e632bbbbae77dcc6b3568ce7df73
Jake Smolowe <jakesmolowe@gmail.com>
2024-07-31 17:29:05 -0700

    Added Chord.Swift, ChordType.swift, changed description of blackKeyFlats and blackKeysharps Enharms, changed isTritone extension to use abs, added suffixes to Suffix for redundant cases, added enharmByKey to Note, fixed keySwitch for E in Root

---
+++ ChordCombiner-SwiftUI/Models/Chords/Chord.swift
@@ -0,0 +1,8 @@
+//
+//  Chord.swift
+//  ChordCombiner-SwiftUI
+//
+//  Created by Jake Smolowe on 7/29/24.
+//
+
+import Foundation
---
+++ ChordCombiner-SwiftUI/Models/Chords/ChordType.swift
@@ -0,0 +1,8 @@
+//
+//  ChordType.swift
+//  ChordCombiner-SwiftUI
+//
+//  Created by Jake Smolowe on 7/31/24.
+//
+
+import Foundation
