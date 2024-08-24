eed3990ffa1226972e55e2e52ef5a71623db0148
Jake Smolowe <jakesmolowe@gmail.com>
2024-08-23 23:10:58 -0700

    Consolidated UpperChord and LowerChord Keyboard Views to SingleChordKeyboardView

---
+++ ChordCombiner-SwiftUI/Views/Keyboard Views/SingleChordKeyboardView.swift
@@ -0,0 +1,66 @@
+//
+//  SingleChordKeyboardView.swift
+//  ChordCombiner-SwiftUI
+//
+//  Created by Jake Smolowe on 8/23/24.
+//
+
+import SwiftUI
+
+struct SingleChordKeyboardView: View {
+  var text: String
+  @Binding var multiChord: MultiChord
+  @Binding var oldMultiChord: MultiChord
+  
+  @State var keyboard: Keyboard
+  var color: Color
+    
+  var body: some View {
+    VStack(spacing: 20) {
+      ChordMenu(
+        text: text,
+        chord: $multiChord.upperChord
+      )
+      keyboard
+    }
+    .onAppear(perform: {
+      ChordHighlighter.highlightStacked(
+        chord: &multiChord.upperChord,
+        oldChord: &oldMultiChord.upperChord,
+        keyboard: &keyboard,
+        initial: true,
+        color: color
+      )
+    })
+    .onChange(of: multiChord.upperChord) {
+      ChordHighlighter.highlightStacked(
+        chord: &multiChord.upperChord,
+        oldChord: &oldMultiChord.upperChord,
+        keyboard: &keyboard,
+        initial: false,
+        color: color
+      )
+    }
+    
+  }
+}
+
+#Preview {
+  SingleChordKeyboardView(
+    text: "Select Chord:",
+    multiChord: Binding.constant(
+      MultiChord(
+        lowerChord: Chord(.c, .ma7, startingOctave: 4),
+        upperChord: Chord(.d, .ma, startingOctave: 4)
+      )
+    ),
+      oldMultiChord: Binding.constant(
+        MultiChord(
+          lowerChord: Chord(.c, .ma7, startingOctave: 4),
+          upperChord: Chord(.d, .ma, startingOctave: 4)
+        )
+      ),
+    keyboard: Keyboard(geoWidth: 351, initialKey: .C,  startingOctave: 4, octaves: 5),
+    color: .yellow
+  )
+}
