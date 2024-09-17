67495a63ad44051d8d167de71d322b13ef6f774d
Jake Smolowe <jakesmolowe@gmail.com>
2024-09-16 17:30:56 -0700

    ChordType: added dominant7_alt_b9_sh9_sh11, added var commonName; Chord: removed chordTypeName, changed name to preciseName and added commonName; added ChordDetailView and ChordMenuButtonTestView; MultiChordKeyboardView: added NavigationStack; DualKeyboardView:  added info symbol with NavigationLink

---
+++ ChordCombiner-SwiftUI/Views/ChordDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ChordDetailView.swift
+//  ChordCombiner-SwiftUI
+//
+//  Created by Jake Smolowe on 9/16/24.
+//
+
+import SwiftUI
+
+struct ChordDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChordDetailView()
+}
---
+++ ChordCombiner-SwiftUI/Views/ChordMenuButtonTestView.swift
@@ -0,0 +1,18 @@
+//
+//  ChordMenuButtonTestView.swift
+//  ChordCombiner-SwiftUI
+//
+//  Created by Jake Smolowe on 9/15/24.
+//
+
+import SwiftUI
+
+struct ChordMenuButtonTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChordMenuButtonTestView()
+}
