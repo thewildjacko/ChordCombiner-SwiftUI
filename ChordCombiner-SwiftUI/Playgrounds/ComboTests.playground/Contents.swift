5a85020ffc44c19f7f0441b782418d44bd3457f5
Jake Smolowe <jakesmolowe@gmail.com>
2024-09-23 15:43:40 -0700

    Added Swift Algorithms package; added functionality to check for inversions based on the upperChord root in ChordFactory.combineChordDegrees, and display resultChord as a slash chord. TODO: fix keyboard display of slash chords! Also add check for inversions based on other notes in resultChord combined degrees

--- ChordCombiner-SwiftUI/Playgrounds/ComboTests.playground/Contents.swift
+++ ChordCombiner-SwiftUI/Playgrounds/ComboTests.playground/Contents.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Algorithms
 
 //for type in ChordType.allChordTypesMinusOmits {
 //  print(type.commonName)
@@ -26,6 +27,71 @@ import SwiftUI
 //for degreeSet in unmatchedChordDegreeSets {
 //  print(degreeSet)
 //}
+
+var multiChord = MultiChord(
+  lowerChord: Chord(.c, .aug, startingOctave: 4),
+  upperChord: Chord(.a, .mi, startingOctave: 4)
+)
+
+multiChord.resultChord = ChordFactory.combineChordDegrees(lowerDegrees: multiChord.multiChordVoicingCalculator.lowerDegrees, upperDegrees: multiChord.multiChordVoicingCalculator.upperDegrees, lowerRoot: multiChord.multiChordVoicingCalculator.lowerRoot, upperRoot: multiChord.multiChordVoicingCalculator.upperRoot)
+
+multiChord.resultChord
+
+
+
+let numbers: [Int] = Array(0...11)
+let numbersCycled = numbers.cycled(times: 2)
+numbersCycled.count
+let numbersCycledTrimmed = numbersCycled.trimmingSuffix { $0 > 1 }
+let numbersCycledWindowed = numbersCycledTrimmed.windows(ofCount: 3)
+
+var combos = numbers.combinations(ofCount: 8)
+combos.count
+var combosFiltered = combos.filter { $0.contains(0) }
+combosFiltered.count
+
+for (combo, window) in product(combosFiltered, numbersCycledWindowed) {
+  let windowArray = Array(window)
+  if combo.includes(windowArray) {
+//    print(combo, windowArray)
+    if let index = combosFiltered.firstIndex(of: combo) {
+      combosFiltered.remove(at: index)
+    }
+  }
+}
+
+combosFiltered
+combosFiltered.count
+
+var all8NoteChordDegrees = ChordType.allChordDegrees.filter { $0.count == 8 }
+
+combosFiltered = combosFiltered.filter { combo in
+  !all8NoteChordDegrees.contains(combo)
+}
+
+for degree in 1...11 {
+  for (index, degrees) in all8NoteChordDegrees.enumerated() {
+    all8NoteChordDegrees[index] = degrees.map { $0.plusDeg(degree) }
+  }
+  
+  for (combo, degrees) in product(combosFiltered, all8NoteChordDegrees) {
+    if combo.includes(degrees) {
+      if let index = combosFiltered.firstIndex(of: combo) {
+        combosFiltered.remove(at: index)
+      }
+    }
+  }
+  
+  all8NoteChordDegrees = ChordType.allChordDegrees.filter { $0.count == 8 }
+}
+
+combosFiltered.count
+for combo in combosFiltered {
+  print(combo)
+}
+
+
+ChordFactory.combos(count: 4)
 
 var chord = Chord(.c, .ma13_sh11, startingOctave: 4)
 print(chord.type.degreeTags)
