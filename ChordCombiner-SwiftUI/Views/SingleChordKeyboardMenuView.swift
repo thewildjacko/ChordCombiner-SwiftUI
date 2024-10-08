feedc6d6ad426a9dcd6ec6bad5d3b0887e8f8e14
Jake Smolowe <jakesmolowe@gmail.com>
2024-10-08 09:31:08 -0700

    Renamed SingleChordKeyboardDisplayView to SingleChordKeyboardMenuView

---
+++ ChordCombiner-SwiftUI/Views/SingleChordKeyboardMenuView.swift
@@ -0,0 +1,57 @@
+//
+//  SingleChordKeyboardMenuView.swift
+//  ChordCombiner-SwiftUI
+//
+//  Created by Jake Smolowe on 9/21/24.
+//
+
+import SwiftUI
+
+struct SingleChordKeyboardMenuView: View {
+  @State var chord: Chord = Chord(.c, .ma, startingOctave: 3)
+  @State var oldChord: Chord = Chord(.c, .ma, startingOctave: 3)
+  @State var keyboard: Keyboard = Keyboard(geoWidth: 300, initialKey: .C,  startingOctave: 4, octaves: 3)
+  @State var isInitial: Bool = true
+  var color: Color = .yellow
+
+  //  MARK: instance methods
+  func setAndHighlightChord() {
+    let stackedPitches = chord.voicingCalculator.stackedPitches
+    
+    if isInitial {
+      keyboard.toggleHighlightKeysSingle(degs: stackedPitches, color: color)
+            
+      isInitial = false
+    } else {
+      let oldStackedPitches = oldChord.voicingCalculator.stackedPitches
+      
+      keyboard.toggleHighlightKeysSingle(degs: oldStackedPitches, color: color)
+      keyboard.toggleHighlightKeysSingle(degs: stackedPitches, color: color)
+    }
+    
+    oldChord = chord
+
+  }
+
+  var body: some View {
+    VStack(spacing: 20) {
+      ChordMenu(
+        text: "Select Chord",
+        chord: $chord,
+        keyboard: $keyboard)
+    }
+    .onAppear(perform: {
+      setAndHighlightChord()
+    })
+    .onChange(of: chord) {
+      setAndHighlightChord()
+    }
+  }
+}
+
+#Preview {
+    SingleChordKeyboardMenuView(
+    chord: Chord(.c, .ma),
+    keyboard: Keyboard(geoWidth: 351, initialKey: .C,  startingOctave: 4, octaves: 3),
+    color: .yellow)
+}
