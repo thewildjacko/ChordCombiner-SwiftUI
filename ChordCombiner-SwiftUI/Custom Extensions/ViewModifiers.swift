926a87a99bcb5f3ef56127f417a8a9a2d2d97b3d
Jake Smolowe <jakesmolowe@gmail.com>
2024-10-12 08:55:32 -0700

    renamed tagViewModifier to roundRectTagViewModifier; added HighlightAbleTagViewModifier, TitleFormatViewModifier, MenuTitleFormatViewModifier, TitleColorOverlay and corresponding view extensions

--- ChordCombiner-SwiftUI/Custom Extensions/ViewModifiers.swift
+++ ChordCombiner-SwiftUI/Custom Extensions/ViewModifiers.swift
@@ -7,8 +7,7 @@
 
 import SwiftUI
 
-struct TagViewModifier: ViewModifier {
-  var condition: Bool
+struct RoundRectTagViewModifier: ViewModifier {
   var font: Font = .title3
   var horizontalPadding: CGFloat = 9
   var verticalPadding: CGFloat = 5
@@ -19,21 +18,108 @@ struct TagViewModifier: ViewModifier {
       .font(font)
       .padding(.horizontal, horizontalPadding)
       .padding(.vertical, verticalPadding)
-      .background(condition ? .tagBackgroundHighlighted : .tagBackground)
-      .foregroundStyle(condition ? .tagTextHighlighted : .tagText)
+      .background(.tagBackground)
+      .foregroundStyle(.title)
       .fontWeight(.bold)
       .clipShape(RoundedRectangle(cornerRadius: cornerRadius, style: .continuous))
   }
 }
 
+struct HighlightableTagViewModifier: ViewModifier {
+  var highlightCondition: Bool
+  var font: Font = .title3
+  var horizontalPadding: CGFloat = 9
+  var verticalPadding: CGFloat = 5
+  var cornerRadius: CGFloat = 8
+  var stroke: Color = .clear
+  
+  func body(content: Content) -> some View {
+    content
+      .font(font)
+      .padding(.horizontal, horizontalPadding)
+      .padding(.vertical, verticalPadding)
+      .background(highlightCondition ? .tagBackgroundHighlighted : .tagBackground)
+      .foregroundStyle(highlightCondition ? .tagTextHighlighted : .tagText)
+      .fontWeight(.bold)
+      .clipShape(
+        RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+      )
+      .overlay(
+        RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+          .fill(.clear)
+          .stroke(stroke, lineWidth: 3)
+      )
+  }
+}
+
+struct TitleFormatViewModifier: ViewModifier {
+  var font: Font = .largeTitle
+  var weight: Font.Weight = .regular
+  
+  func body(content: Content) -> some View {
+    content
+      .font(font)
+      .fontWeight(weight)
+      .foregroundStyle(.title)
+      .fixedSize()
+      .fontWeight(.heavy)
+  }
+}
+
+struct MenuTitleFormatViewModifier: ViewModifier {
+  var font: Font = .largeTitle
+  var weight: Font.Weight = .regular
+  
+  func body(content: Content) -> some View {
+    content
+      .font(font)
+      .fontWeight(weight)
+      .fixedSize()
+      .fontWeight(.heavy)
+  }
+}
+
+struct TitleColorOverlay: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .overlay(.title)
+  }
+}
+
 extension View {
-  func tagView(condition: Bool, font: Font = .title3, horizontalPadding: CGFloat = 9, verticalPadding: CGFloat = 5, cornerRadius: CGFloat = 8) -> some View {
-    modifier(TagViewModifier(
-      condition: condition,
-      font: font,
-      horizontalPadding: horizontalPadding,
-      verticalPadding: verticalPadding,
-      cornerRadius: cornerRadius)
+  func roundRectTagView(font: Font = .title3, horizontalPadding: CGFloat = 9, verticalPadding: CGFloat = 5, cornerRadius: CGFloat = 8) -> some View {
+    modifier(
+      RoundRectTagViewModifier(
+        font: font,
+        horizontalPadding: horizontalPadding,
+        verticalPadding: verticalPadding,
+        cornerRadius: cornerRadius
+      )
     )
+  }
+  
+  func highlightableTagView(highlightCondition: Bool, font: Font = .title3, horizontalPadding: CGFloat = 9, verticalPadding: CGFloat = 5, cornerRadius: CGFloat = 8, stroke: Color = .clear) -> some View {
+    modifier(
+      HighlightableTagViewModifier(
+        highlightCondition: highlightCondition,
+        font: font,
+        horizontalPadding: horizontalPadding,
+        verticalPadding: verticalPadding,
+        cornerRadius: cornerRadius,
+        stroke: stroke
+      )
+    )
+  }
+  
+  func titleFormat(font: Font = .largeTitle, weight: Font.Weight = .regular) -> some View {
+    modifier(TitleFormatViewModifier(font: font, weight: weight))
+  }
+  
+  func menuTitleFormat(font: Font = .largeTitle, weight: Font.Weight = .regular) -> some View {
+    modifier(MenuTitleFormatViewModifier(font: font, weight: weight))
+  }
+  
+  func titleColorOverlay() -> some View {
+    modifier(TitleColorOverlay())
   }
 }
