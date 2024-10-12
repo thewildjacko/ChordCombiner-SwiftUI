09333b201f684e9499e124645474cbb2f245bc6b
Jake Smolowe <jakesmolowe@gmail.com>
2024-10-12 09:26:25 -0700

    refactored to only contain the ScrollView and not the title, and also to use reusable views

--- ChordCombiner-SwiftUI/Views/AllChords/KeyboardDisplayScrollView.swift
+++ ChordCombiner-SwiftUI/Views/AllChords/KeyboardDisplayScrollView.swift
@@ -1,6 +1,6 @@
 //
 //  KeyboardDisplayScrollView.swift
-//  ChordCombiner-SwiftUI
+//  ComboTests
 //
 //  Created by Jake Smolowe on 10/2/24.
 //
@@ -10,101 +10,50 @@ import SwiftUI
 struct KeyboardDisplayScrollView: View {
   let section: ChordTypeSection
   @Binding var rootKeyNote: RootKeyNote
-  let isExpanded: Bool
-  @Binding var expandedSections: Set<String>
-  @State var kbDisplay: Bool = false
-  
+  @Binding var kbDisplay: Bool
+
   let rows = [
     GridItem(.flexible()),
     GridItem(.flexible()),
     GridItem(.flexible())
   ]
   
-  var body: some View {
-    content
-  }
-  
-  func selectDeselect(_ section: ChordTypeSection) {
-    if expandedSections.contains(section.tagName) {
-      expandedSections.remove(section.tagName)
-    } else {
-      expandedSections.insert(section.tagName)
-    }
-  }
-  
-  private var content: some View {
-    VStack(alignment: .leading) {
-      HStack {
-        HStack {
-          if !isExpanded {
-            Image(systemName: "chevron.right")
-          } else {
-            Image(systemName: "chevron.down")
-          }
-          Text(section.title)
-          Spacer()
-        }
-        .font(.headline)
-        .fontWeight(.bold)
-        .contentShape(Rectangle())
-        .onTapGesture {
-          withAnimation(.easeInOut) {
-            self.selectDeselect(section)
-          }
-        }
-        if isExpanded {
-          HStack {
-            Keyboard(geoWidth: 40, keyCount: 12, initialKey: .C, startingOctave: 4)
-              .padding(.trailing, 5)
-              .onTapGesture {
-                kbDisplay.toggle()
-              }
-            Toggle("", isOn: $kbDisplay)
-              .labelsHidden()
-          }
-        }
-      }
-      
-      if isExpanded {
-        ScrollView(.horizontal) {
-          LazyHGrid(rows: rows, spacing: 20) {
-            ForEach(section.chordTypes) { type in
-              let chord = Chord(rootKeyNote, type)
-              
-              if kbDisplay {
-                KeyboardDisplayView(
-                  chord: chord,
-                  keyboard: Keyboard(
-                    geoWidth: 250,
-                    initialKey: .C,
-                    startingOctave: 4,
-                    octaves: 3),
-                  color: .yellow
+    var body: some View {
+      ScrollView(.horizontal) {
+        LazyHGrid(rows: rows, spacing: 20) {
+          ForEach(section.chordTypes) { type in
+            let chord = Chord(rootKeyNote, type)
+            
+            if kbDisplay {
+              KeyboardDisplayView(
+                chord: chord,
+                keyboard: Keyboard(
+                  geoWidth: 250,
+                  initialKey: .C,
+                  startingOctave: 4,
+                  octaves: 3),
+                color: .yellow
+              )
+            } else {
+              Text(chord.preciseName)
+                .roundRectTagView(
+                  font: .title2,
+                  horizontalPadding: 8,
+                  verticalPadding: 5,
+                  cornerRadius: 12
                 )
-              } else {
-                Text(chord.preciseName)
-                  .font(.title2)
-                  .padding(.horizontal, 8)
-                  .padding(.vertical, 5)
-                  .background(Color(red: 0.8, green: 0.8, blue: 0.8, opacity: 0.9))
-                  .foregroundStyle(.title)
-                  .fontWeight(.bold)
-                  .clipShape(RoundedRectangle(cornerRadius: 12.0, style: .continuous))
-              }
             }
           }
         }
-        .frame(maxHeight: .infinity)
       }
+      .frame(maxHeight: .infinity)
     }
-    .padding(8)
-  }
 }
 
 #Preview {
-  KeyboardDisplayScrollView(section: ChordType.chordTypeSections[5],
-                            rootKeyNote: Binding.constant(.c),
-                            isExpanded: true,
-                            expandedSections: Binding.constant([]))
-  .padding()
+    KeyboardDisplayScrollView(
+      section: /*Binding.constant(*/ChordType.chordTypeSections[5]/*)*/,
+      rootKeyNote: Binding.constant(.c),
+      kbDisplay: Binding.constant(true)
+    )
 }
