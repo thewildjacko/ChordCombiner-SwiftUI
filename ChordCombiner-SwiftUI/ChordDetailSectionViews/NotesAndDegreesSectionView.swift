008da0f74a8584437cddbbc159f2ffd1251534c1
Jake Smolowe <jakesmolowe@gmail.com>
2025-07-07 13:18:35 -0700

    added struct ScaleNotesAndDegreesSectionView

--- ChordCombiner-SwiftUI/ChordDetailSectionViews/NotesAndDegreesSectionView.swift
+++ ChordCombiner-SwiftUI/ChordDetailSectionViews/NotesAndDegreesSectionView.swift
@@ -12,11 +12,23 @@ struct NotesAndDegreesSectionView: View {
   let degreesText: String
   @Binding var chordGrapherNavigationView: ChordGrapherNavigationView
 
-    var body: some View {
-      List {
-        DetailRow(title: "Notes", text: notesText)
-        DetailRow(title: "Degrees", text: degreesText)
-        chordGrapherNavigationView
-      }
+  var body: some View {
+    List {
+      DetailRow(title: "Notes", text: notesText)
+      DetailRow(title: "Degrees", text: degreesText)
+      chordGrapherNavigationView
     }
+  }
+}
+
+struct ScaleNotesAndDegreesSectionView: View {
+  let notesText: String
+  let degreesText: String
+
+  var body: some View {
+    List {
+      DetailRow(title: "Notes", text: notesText)
+      DetailRow(title: "Degrees", text: degreesText)
+    }
+  }
 }
