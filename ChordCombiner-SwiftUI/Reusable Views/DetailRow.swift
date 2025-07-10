334aaff291fa5d0f8d95886e0b0caec615e77e03
Jake Smolowe <jakesmolowe@gmail.com>
2025-07-10 13:14:31 -0700

    added var color

--- ChordCombiner-SwiftUI/Reusable Views/DetailRow.swift
+++ ChordCombiner-SwiftUI/Reusable Views/DetailRow.swift
@@ -10,20 +10,23 @@ import SwiftUI
 struct DetailRow: View {
   var title: String
   var text: String
+  var color: Color = .title
 
   var body: some View {
     HStack {
       TitleView(
         text: title,
         font: .body,
-        weight: .bold)
+        weight: .bold,
+        color: color)
 
       Spacer()
 
       TitleView(
         text: text,
         font: .body,
-        weight: .regular)
+        weight: .regular,
+        color: color)
     }
   }
 }
