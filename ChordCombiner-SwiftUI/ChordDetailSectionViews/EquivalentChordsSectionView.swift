e9013adb8741cb5676983b3ee0fe28050a7099f4
Jake Smolowe <jakesmolowe@gmail.com>
2025-07-10 13:23:54 -0700

    chord no longer optional

--- ChordCombiner-SwiftUI/ChordDetailSectionViews/EquivalentChordsSectionView.swift
+++ ChordCombiner-SwiftUI/ChordDetailSectionViews/EquivalentChordsSectionView.swift
@@ -9,26 +9,24 @@ import SwiftUI
 
 struct EquivalentChordsSectionView: View {
   var keyboardWidth: CGFloat = 351
-  let chord: Chord?
+  let chord: Chord
 
   @ViewBuilder
-    var body: some View {
-      if let chord = chord {
-        if !chord.getEquivalentChords().isEmpty {
-          Section(header: Text("Equivalent Chords")) {
-            List {
-              ForEach(chord.getEquivalentChords()) { chord in
-                SingleChordDetailNavigationView(
-                  keyboardWidth: keyboardWidth,
-                  chord: chord,
-                  color: .lowerChordHighlight,
-                  labelType: .title)
-              }
-            }
+  var body: some View {
+    if !chord.getEquivalentChords().isEmpty {
+      Section(header: Text("Equivalent Chords")) {
+        List {
+          ForEach(chord.getEquivalentChords()) { chord in
+            SingleChordDetailNavigationView(
+              keyboardWidth: keyboardWidth,
+              chord: chord,
+              color: .lowerChordHighlight,
+              labelType: .title)
           }
         }
       }
     }
+  }
 }
 
 #Preview {
